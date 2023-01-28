cd1a9458f2c7ebc8db8cb031b72a9886b2f6f0b9
rmsliap <rmsliap@gmail.com>
2023-01-28 12:21:54 +0300

    Add changing planets functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
 import { data } from "./data";
 
-document.querySelector;
+const navLis = document.querySelectorAll("li");
+
+navLis.forEach((navLi) => {
+  navLi.addEventListener("click", (event) => {
+    targetPlanet(event);
+  });
+});
+
+// Target planet from data array
+function targetPlanet(e) {
+  const [planet] = data.filter(
+    (el) => e.target.textContent.toLowerCase() === el.name.toLowerCase()
+  );
+  // console.log(planet);
+  renderInfo(planet);
+}
+
+function renderInfo(planet) {
+  const title = document.querySelector(".planet-title");
+  const desc = document.querySelector(".planet-desc");
+  const source = document.querySelector(".planet-source a");
+  const planetImg = document.querySelector(".planet-img");
+
+  title.textContent = planet.name;
+  desc.textContent = planet.overview.content;
+  source.href = planet.overview.source;
+  planetImg.src = planet.images.planet;
+}
