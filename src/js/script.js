975c56a9305e8627c8c1b072cddf6c52d01f2dc6
rmsliap <rmsliap@gmail.com>
2023-01-28 22:21:55 +0300

    Add geology images

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,17 @@
 import data from "../data.json" assert { type: "json" };
 import { renderInfo } from "./renderInfo.js";
-import { changeActive } from "./changeActive.js";
-
-const navLis = document.querySelectorAll("li");
-
-navLis.forEach((navLi) => {
-  navLi.addEventListener("click", (event) => {
-    targetPlanet(event);
-  });
-});
-
-function targetPlanet(e) {
-  const [planet] = data.filter(
-    (el) => e.target.textContent.toLowerCase() === el.name.toLowerCase()
-  );
-  renderInfo(planet);
-  changeActive(planet);
-}
+import { changeActive, firstTabActive } from "./changeActive.js";
 
 changeActive(data[0]);
+
+const navLis = document.querySelectorAll("li");
+navLis.forEach((navLi) => {
+  navLi.addEventListener("click", (e) => {
+    const [planet] = data.filter(
+      (el) => e.target.textContent.toLowerCase() === el.name.toLowerCase()
+    );
+    renderInfo(planet);
+    firstTabActive();
+    changeActive(planet);
+  });
+});
