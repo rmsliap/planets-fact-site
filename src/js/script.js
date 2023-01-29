4dd8ef0fd4c132d0cd52f604a32027acc18c2562
rmsliap <rmsliap@gmail.com>
2023-01-29 18:07:10 +0300

    Add hamburger menu

--- src/js/script.js
+++ src/js/script.js
@@ -11,11 +11,8 @@ navLis.forEach((navLi) => {
     const [planet] = data.filter(
       (el) => e.target.textContent.toLowerCase() === el.name.toLowerCase()
     );
-    const [{ color }] = colors.filter(
-      (el) => e.target.textContent.toLowerCase() === el.planet.toLowerCase()
-    );
     renderInfo(planet);
-    firstTabActive(color);
-    changeActive(planet, color);
+    firstTabActive();
+    changeActive(planet);
   });
 });
