51c8e8cad3021dd0a7b869b0a9a036bc527c1e10
rmsliap <rmsliap@gmail.com>
2023-01-29 21:36:43 +0300

    Add mobile view and planet colors

--- js/changeActive.js
+++ js/changeActive.js
@@ -2,24 +2,23 @@ import { updateDesc } from "./updateDesc.js";
 
 const tabs = document.querySelectorAll(".tab");
 
-function changeActive(planet, color) {
+function changeActive(planet) {
   tabs.forEach((tab) => {
     tab.addEventListener("click", function (e) {
       tabs.forEach((tab) => {
-        tab.classList.remove("active");
+        tab.style.backgroundColor = "transparent";
       });
-      e.target.classList.add("active");
-      // Update Description
+      e.target.style.backgroundColor = planet.color;
       updateDesc(planet, e.target);
     });
   });
 }
 
-function firstTabActive(color) {
+function firstTabActive(planet) {
   tabs.forEach((tab) => {
-    tab.classList.remove("active");
+    tab.style.backgroundColor = "transparent";
   });
-  tabs[0].classList.add("active");
+  tabs[0].style.backgroundColor = planet.color;
   const geologyImg = document.querySelector(".geology-img");
   geologyImg.style.display = "none";
 }
--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
 import data from "../data.json" assert { type: "json" };
 import { renderInfo } from "./renderInfo.js";
 import { changeActive, firstTabActive } from "./changeActive.js";
-import { colors } from "./activeColors.js";
 
 changeActive(data[0]);
 
 const navLis = document.querySelectorAll("li");
+const hgIcon = document.querySelector(".hb-icon");
+const navList = document.querySelector(".nav-list");
+
 navLis.forEach((navLi) => {
   navLi.addEventListener("click", (e) => {
     const [planet] = data.filter(
       (el) => e.target.textContent.toLowerCase() === el.name.toLowerCase()
     );
     renderInfo(planet);
-    firstTabActive();
+    firstTabActive(planet);
     changeActive(planet);
+    navList.classList.add("hidden");
   });
+});
+
+hgIcon.addEventListener("click", function () {
+  navList.classList.toggle("hidden");
+  console.log("Clicked");
 });
